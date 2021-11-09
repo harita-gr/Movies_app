179a303cf7e56ce393d5efe404833883c8c7cdc7
Harita Ravindranath <harrysama01@outlook.com>
2021-11-09 11:53:13 +0530

    app structure setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import './App.scss';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes, //replaces "Switch"
+  Route,
+} from "react-router-dom";
+import "./App.scss";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <Router>
+        <Header></Header>
+        <div className="container">
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movie/:imdbID" element={<MovieDetail />} />
+            <Route path="*" element={<PageNotFound />} />
+        </Routes>
+        </div>
+        <Footer />
+      </Router>
     </div>
   );
 }
